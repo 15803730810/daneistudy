bb8eecdc24c1d0d984b0ab51f086b974b94ace18
15803730810 <15803730810@163.com>
2019-11-23 22:43:06 +0800

    冒泡排序和双色球

---
+++ duoblecolor.js
@@ -0,0 +1,30 @@
+// 1-33随机生成不重复的6个数字作为红球，从小到大排序，随机生成1-16之中的一个，与之前6个红球组合到一起，并放到最后
+// 随机生成1-33个数放入数组
+// 声明一个数组用于存放33个数字
+let arr=[];
+// 声明一个数组用于存放红球
+let arr1=[];
+for (var i = 1; i < 34; i++) {
+	arr.push(i);
+}
+//循环取出数组中的6个数字
+for (var i = 0; i < 6; i++) {
+	let index=Math.floor(Math.random()*arr.length);
+	arr1.push(arr[index]);
+	arr.splice(index, 1);
+}
+// 声明一个变量用于存放篮球
+let blue=Math.floor(Math.random()*16+1);
+// 将红球排序
+arr1.sort((a,b)=>a-b);
+// 将红球篮球放一起
+arr1.push(blue);
+// 打印随机生成的双色球；
+console.log(arr1);
+
+
+
+
+
+
+
